e13c36c9193482bc84c63066ce8764c30271e0b1
Stig Tanggaard <stigcq@yahoo.com>
2018-10-23 11:22:21 +0200

    testing db

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testDbRouter = require('./routes/testDb');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/testdb', testDbRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/testDb.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+var MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@localhost:27017', function (err, client) {
+  if (err) throw err
+
+  var db = client.db('livefeed-api');
+
+  /*db.collection('message').find().toArray(function (err, result) {
+      if (err) throw err
+  
+      console.log(result)
+    });*/
+
+    db.createCollection("message", function(err, res) {
+        if (err) throw err;
+        console.log("Collection created!");
+        db.close();
+      });
+  
+});
+
+/*  */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource test');
+});
+
+module.exports = router;
