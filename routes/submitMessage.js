62cf12ac98c499d3fe6067c690ce627af94763a2
Stig Tanggaard <stigcq@yahoo.com>
2018-10-23 13:13:20 +0200

    add message to db

--- routes/submitMessage.js
+++ routes/submitMessage.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/*var MongoClient = require('mongodb').MongoClient
+var MongoClient = require('mongodb').MongoClient
 
-console.log('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@localhost:27017');
+/*console.log('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@localhost:27017');
 
 MongoClient.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@localhost:27017', function (err, client) {
   if (err) throw err
@@ -20,7 +20,28 @@ MongoClient.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PA
 
 /*  */
 router.post('/', function(req, res, next) {
-  res.send('Submit message ' + req.body.beer + " " + req.body.rating);
+
+    const thread_id = req.body.thread_id;
+    const content = req.body.content;
+
+    var item = {"thread_id": thread_id, "content": content, "": 1};
+    
+    MongoClient.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + 
+        '@localhost:27017', function (err, client) {
+        if (err) throw err
+      
+        var db = client.db('livefeed-api');
+
+        db.collection("message").insertOne(item, 
+            function(err, ires) {
+            if (err) throw err;
+            console.log("1 message inserted");
+            client.close();
+            res.send( 'message added ' + item._id);
+        });
+    });
+
+  res.send('Submit message ' + req.body.thread_id + " " + req.body.content);
 });
 
 module.exports = router;
