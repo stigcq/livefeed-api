8ba71309f38fd1517a72a480021340630779d5ae
Stig Tanggaard <stigcq@yahoo.com>
2018-10-23 12:03:10 +0200

    test json input

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ console.log(process.env.DOTENV_HELLO);
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testDbRouter = require('./routes/testDb');
+var submitMessageRouter = require('./routes/submitMessage');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testdb', testDbRouter);
+app.use('/submit_message', submitMessageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/submitMessage.js
+++ routes/submitMessage.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var MongoClient = require('mongodb').MongoClient
+/*var MongoClient = require('mongodb').MongoClient
 
 console.log('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@localhost:27017');
 
@@ -10,23 +10,17 @@ MongoClient.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PA
 
   var db = client.db('livefeed-api');
 
-  /*db.collection('message').find().toArray(function (err, result) {
-      if (err) throw err
-  
-      console.log(result)
-    });*/
-
     db.createCollection("message", function(err, res) {
         if (err) throw err;
         console.log("Collection created!");
         client.close();
       });
   
-});
+});*/
 
 /*  */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource test');
+router.post('/', function(req, res, next) {
+  res.send('Submit message' + req.body);
 });
 
 module.exports = router;
