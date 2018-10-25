8ce23673b1a2e8119c9a017cd205e708166b7b65
Stig Tanggaard <stigcq@yahoo.com>
2018-10-25 18:01:07 +0200

    test

--- routes/checkUserLoggedIn.js
+++ routes/checkUserLoggedIn.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.all('/', function(req, res, next) {
 
   const session_token = req.body.session_token;
 
