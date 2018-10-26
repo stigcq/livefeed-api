0f6635c00d604b2d547b9015e2413b4bb443dd42
Stig Tanggaard <stigcq@yahoo.com>
2018-10-26 13:07:23 +0200

    testing

--- routes/deleteMessage.js
+++ routes/deleteMessage.js
@@ -6,25 +6,24 @@ var ObjectID = require('mongodb').ObjectID;
 
 
 /* delete message */
-router.get('/', function(req, res, next) {
+router.get('/:message_id', function(req, res, next) {
 
     const message_id = req.params.message_id;
     
     
     MongoClient.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + 
-        '@localhost:27017', function (err, client) {
+        '@' + process.env.DB_HOST + ':27017', function (err, client) {
         if (err) throw err
       
         var db = client.db('livefeed-api');
 
         var query = {"_id": ObjectID(message_id)};
 
-        db.collection("message").deleteOne(query, 
-            function(err, ires) {
+        db.collection("message").deleteOne(query, function(err, ires) {
             if (err) throw err;
             console.log("1 message deleted id " + message_id);
             client.close();
-            res.send( 'message deleted ');
+            res.send(JSON.stringify(ires));
         });
     });
 
