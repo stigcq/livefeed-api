306dc4a1051efebbf6e00cb2dc7c134620eeb235
Stig Tanggaard <stigcq@yahoo.com>
2018-10-27 18:11:40 +0200

    test

--- routes/getFeed.js
+++ routes/getFeed.js
@@ -33,7 +33,7 @@ router.get('/:feed_id/:feed_time?/:goback?', function(req, res, next) {
             }
           }
         
-        db.collection("message").aggregate(aggregate).find(myquery).sort(mysort).toArray(function(err, result) {
+        db.collection("message").find(myquery).aggregate(aggregate).sort(mysort).toArray(function(err, result) {
             if (err) throw err;
             console.log("No rows found: " + result.length);
             client.close();
