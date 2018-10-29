f4b75e5f0ddf6b53b6c40faf38eea889e34166b6
Stig Tanggaard <stigcq@yahoo.com>
2018-10-29 22:13:34 +0100

    refactor + error codes

--- routes/getMessage.js
+++ routes/getMessage.js
@@ -1,44 +1,31 @@
 var express = require('express');
 var router = express.Router();
-
-var MongoClient = require('mongodb').MongoClient
-var ObjectID = require('mongodb').ObjectID;
+var dataLayer = require('./../lib/DataLayer');
+var mongodb = require("mongodb");
 
 /*  */
 router.get('/:message_id', function(req, res, next) {
 
     const message_id = req.params.message_id;
 
-    if(message_id.length != 12 && message_id.length != 24) {
-
-        res.send("{'error': 1, 'mesage': 'message id is not valid' }");
+    if(mongodb.ObjectID.isValid(message_id) == false) {
+        response = {'error': 6, 'mesage': 'message id is not valid' };
+        res.send(JSON.stringify(response));
         return;
     }
     
     console.log("get_message " + message_id);
 
-    MongoClient.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + 
-        '@' + process.env.DB_HOST + ':' + process.env.DB_PORT, function (err, client) {
-        if (err) throw err
-      
-        var db = client.db('livefeed-api');
+    dataLayer.getMessage(message_id, function(message) {
 
-        db.collection("message").findOne({ _id: ObjectID(message_id) }, 
-            function(err, ires) {
-            if (err) throw err;
-
-            if(ires != null) {
-                console.log("1 message found id " + ires._id );
-                client.close();
-                res.send(JSON.stringify(ires));
-            } else {
-                console.log("no message found ");
-                client.close();
-                res.send("{'error': 1, 'mesage': 'message not found' }");
-            }
-
-        });
+        if(message == false) {
+            response = {'error': 7, 'mesage': 'message not found' };
+            res.send(JSON.stringify(response));
+        } else {
+            res.send(JSON.stringify(message));
+        }
     });
+
 
 });
 
