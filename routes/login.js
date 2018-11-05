af154532e418d2cb766661857a1b78071458998d
Stig Tanggaard <stigcq@yahoo.com>
2018-11-05 13:41:46 +0100

    refactoring

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var MongoClient = require('mongodb').MongoClient
-var ObjectID = require('mongodb').ObjectID;
+var dataLayer = require('./../lib/DataLayer');
 
 /*      
 check email/pass match
@@ -14,44 +13,17 @@ router.post('/', function(req, res, next) {
   const myemail = req.body.email;
   const md5password = req.body.md5password;
 
-  MongoClient.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + 
-  '@' + process.env.DB_HOST + ':27017', function (err, client) {
-  if (err) throw err
+  dataLayer.login(myemail, md5password, function(result) {
 
-  var db = client.db('livefeed-api');
+    if(result == false) {
 
-  db.collection("user").findOne({email: myemail, password: md5password }, 
-      function(err, ires) {
-      if (err) throw err;
+      const response = {message: "No such user", sesson_token: 1};
+      res.send(JSON.stringify(response));
 
-      if(ires != null) {
-        console.log("1 user found id " + ires._id );
-        console.log(JSON.stringify(ires));
-          
-        my_session_token = Math.floor((Math.random() * 10000000));
+    } else {
+        res.send(JSON.stringify(result));
 
-        // FIXME: this query is wrong but I cant get the one using id to work, total BS
-        var myquery = { email: myemail, password: md5password }; //{ '_id': ObjectID(ires.id) };
-        var newvalues = { $set: {'session_token': Number(my_session_token) } };
-
-        db.collection("user").updateOne(myquery, newvalues, function(err, iires) {
-            if (err) throw err;
-            console.log("1 user updated " + my_session_token);
-
-            var myresult = { session_token: my_session_token, _id: ires._id };
-        
-            res.send(JSON.stringify(myresult));
-
-            client.close();
-          });
-
-      } else {
-          console.log("no user found ");
-          client.close();
-          res.send("{'message': 'No user found', 'session_token': 1}");
-      }
-
-    });
+    }
   });
 
 });
