5eaca772247b6450ee0fed140d85ebfbdde496e4
Stig Tanggaard <stigcq@yahoo.com>
2018-11-06 20:17:44 +0100

    fix

--- tests/test.js
+++ tests/test.js
@@ -152,13 +152,13 @@ class GetUserFeedsTest extends Test {
 
         jQuery.get(testUrl + "get_user_feeds/" + loggedInUserObject._id, {}, function(result) {
         
-            userFeeds = jQuery.parseJSON( result );
+            var userFeeds = jQuery.parseJSON( result );
         
-            if(feedObject.error == 1) {
+            if(userFeeds.error == 1) {
                 logTest(false, "GetUserFeedsTest: failed get feeds" + result);
             } else {
 
-                if(feedObject.length == 1)
+                if(userFeeds.length == 1)
                     logTest(true, "GetUserFeedsTest: fetched users feeds" + result);
                 else
                     logTest(false, "GetUserFeedsTest: fetched users feeds count dont match" + result);
