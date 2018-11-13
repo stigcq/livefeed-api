19acd37529225a5893ceab9e21862f0d2de90255
Stig Tanggaard <stigcq@yahoo.com>
2018-11-13 19:29:52 +0100

    test fixes

--- tests/suite1/DeleteFeedTest.js
+++ tests/suite1/DeleteFeedTest.js
@@ -18,14 +18,18 @@ class DeleteFeedTest extends Test {
         test() {
 
             jQuery.post(testUrl + "delete_feed/", 
-            { feed_id: feedObject._id, 
+            { feed_id: feedObject.id, 
                 session_token: loggedInUserObject.session_token }, function(data) {
         
                 feedObjectDeleted = jQuery.parseJSON( data );
 
-
-                logTest(true, "DeleteFeedTest: feed deleted"+ data);    
-
+                if(feedObjectDeleted.error != undefined)
+                    logTest(false, "DeleteFeedTest: feed not deleted"+ data);    
+                else if(feedObjectDeleted.n == 1 && feedObjectDeleted.ok == 1)
+                    logTest(true, "DeleteFeedTest: feed deleted"+ data);    
+                ekse
+                    logTest(false, "DeleteFeedTest: feed not deleted"+ data);    
+                
             }).error(function(conn, message, error) {
                 feedObjectDeleted = true;
                 logTest(false, "DeleteFeedTest: Error in connection <b>" + error + "</b>");
