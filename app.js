02b8bb954177b1c1ad1668edcc09f748aaf18c37
Stig Tanggaard <stigcq@yahoo.com>
2018-12-13 15:31:26 +0100

    test

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+/*app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -94,6 +94,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.send(err.message);
-});
+});*/
 
 module.exports = app;
